e2c016ab6a9504783d74a4c5e8cdaff0bb6286e8
José Luiz <157260669+Luizz694@users.noreply.github.com>
2025-03-21 11:06:30 -0300

    LeetCode

---
+++ SearchInsertPosition.py
@@ -0,0 +1,20 @@
+nums = [1,3,5,6]
+target = 3
+i = 0
+
+while i < len(nums):
+    if nums[i] == target:
+        arm = i
+        break
+
+    if  i == len(nums)-1:
+        for k in range(len(nums)):
+            if nums[k] > target:
+                arm = k
+                break
+            else:
+                arm = k
+    i=i+1
+
+print(arm)
+
